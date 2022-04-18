4862b0b75ca59d0bca9c0e347bc4d905232bc7f2
Erin Ewart <ems1980@hotmail.com>
2022-04-17 23:38:16 -0400

    queried the database to test connecttion

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,11 +7,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: '',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Adv83839!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+console.log(rows);
+});
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
