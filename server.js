3707e9379d857fb56f3e914a2a02c172bb49726a
Erin Ewart <ems1980@hotmail.com>
2022-04-18 11:32:22 -0400

    create and populate a parties table

--- server.js
+++ server.js
@@ -99,19 +99,6 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
-// // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
-
-
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
